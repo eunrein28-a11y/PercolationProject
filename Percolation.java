6cc3cf326eab6573180ac5ce475e3c2106a9e35b
eunrein28 <eunrein28@jesuitmail.org>
2026-09-18 13:39:23 -0700

    Day 1 work (I have no idea what I'm doing at this point)

--- Percolation.java
+++ Percolation.java
@@ -1,7 +1,43 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int n;
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+    private boolean[][] grid;
+    public Percolation(int n){
+        if(n <= 0){
+            throw new IllegalArgumentException("Error! Grid must have dimensions greater than zero!");
+        }
+        this.n = n;
+        this.grid = new boolean[n][n];
+        this.weightedQuickUnionUF = new WeightedQuickUnionUF(n * n);
+    }
+    public void open(int row, int col){
+        if(row > n - 1 || col > n - 1 || col <= 0 || row <= 0){
+            throw new IllegalArgumentException("Error! Index outside its prescribed range [1,n-1]!");
+        }
+        grid[row][col] = true;
+        if(grid[row - 1][col - 1]){
 
+        }
+    }
+    public boolean isOpen(int row, int col){
+        return grid[row][col];
+    }
+    public int numberOfOpenSites(){
+        int count = 0;
+        for(boolean[] row : grid){
+            for(boolean cell : row){
+                if(cell){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public boolean percolates(){
+        return false;
+    }
 
     public static void main(String[] args) {
 
--- PercolationStats.java
+++ PercolationStats.java
@@ -2,7 +2,24 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-
+    private Percolation percolation;
+    private int trials;
+public PercolationStats(int n, int trials){
+    percolation = new Percolation(n);
+    this.trials = trials;
+}
+public double mean(){
+    return 0.0;
+}
+public double stddev(){
+    return 0.0;
+}
+public double confidenceLo(){
+    return 0.0;
+}
+public double confidenceHi(){
+    return 0.0;
+}
 
     public static void main(String[] args) {
 
